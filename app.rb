00a24d70e1455e93a88c363996350916d3c22655
MoniqueTheogene123 <39348407+MoniqueTheogene123@users.noreply.github.com>
2025-07-01 18:02:15 +0000

    commit

--- app.rb
+++ app.rb
@@ -9,32 +9,16 @@ require "http"
 # define a route for the homepage
 get("/") do
 
-  # Assemble the API url, including the API key in the query string
-  api_url = "https://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_RATE_KEY")}"
+    @raw_response = HTTP.get("https://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_RATE_KEY")}")
 
-  # Use HTTP.get to retrieve the API data
-  @raw_response = HTTP.get(api_url)
+    @string_response = @raw_response.to_s
+    @parsed_response = JSON.parse(@string_response)
+    @currencies = @parsed_response.fetch("currencies")
 
-  # Get the body of the response as a string
-  @raw_string = @raw_response.to_s
 
-  # Convert the string to JSON
-  @parsed_data = JSON.parse(@raw_string)
-
-  # Render a view template
   erb(:homepage)
-
-  # In the view template, embed the @parsed_data variable to
-  # see what you're working with.
-  
-  # From there, use your Hash/Array skills to make the homepage 
-  # match the target.
-  
-  # Remember to Make The Invisible Visible — View Source in 
-  # Chrome to see what your templates are actually outputting, 
-  # and embed as many instance variables as you need to (this 
-  # is the new equivalent of pretty-printing everything).
 end
+
 
 get("/:from_currency") do
   @original_currency = params.fetch("from_currency")
