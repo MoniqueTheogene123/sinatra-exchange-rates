940e3dac729829381b32377149146c4d6b640042
MoniqueTheogene123 <39348407+MoniqueTheogene123@users.noreply.github.com>
2025-07-01 16:09:49 +0000

    restarting server

--- app.rb
+++ app.rb
@@ -1,9 +1,38 @@
+# /app.rb
+
 require "sinatra"
 require "sinatra/reloader"
 
+# Pull in the HTTP class
+require "http"
+
+# define a route for the homepage
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
-end
+
+
+
+  # Assemble the API url, including the API key in the query string
+  api_url = "https://api.exchangerate.host/list?access_key=3f7fcf1d090548ea5f5ab1de8c9c6a4f"
+
+  # Use HTTP.get to retrieve the API data
+  @raw_response = HTTP.get(api_url)
+
+  # Get the body of the response as a string
+  @raw_string = @raw_response.to_s
+
+  # Convert the string to JSON
+  @parsed_data = JSON.parse(@raw_string)
+
+  # Render a view template
+  erb(:homepage)
+
+  # In the view template, embed the @parsed_data variable to
+  # see what you're working with.
+  
+  # From there, use your Hash/Array skills to make the homepage 
+  # match the target.
+  
+  # Remember to Make The Invisible Visible — View Source in 
+  # Chrome to see what your templates are actually outputting, 
+  # and embed as many instance variables as you need to (this 
+  # is the new equivalent of pretty-printing everything).
