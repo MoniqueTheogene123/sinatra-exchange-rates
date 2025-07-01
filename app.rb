ab00b25a9b33ce68e2a4566093e688ff5a2a2c1d
MoniqueTheogene123 <39348407+MoniqueTheogene123@users.noreply.github.com>
2025-07-01 19:39:06 +0000

    trying to use puts statements to problemsolve

--- app.rb
+++ app.rb
@@ -9,32 +9,34 @@ require "http"
 # define a route for the homepage
 get("/") do
 
+
     @raw_response = HTTP.get("https://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_RATE_KEY")}")
+    puts "my env exchange rate key: #{ENV.fetch("EXCHANGE_RATE_KEY")}"
 
     @string_response = @raw_response.to_s
     @parsed_response = JSON.parse(@string_response)
     @currencies = @parsed_response.fetch("currencies")
 
-
+    puts @raw_response
   erb(:homepage)
 end
 
 
-get("/:from_currency") do
-  @original_currency = params.fetch("from_currency")
+# get("/:from_currency") do
+#   @original_currency = params.fetch("from_currency")
 
-  api_url = "https://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_RATE_KEY")}"
+#   api_url = "https://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_RATE_KEY")}"
   
-  # Some more code to parse the URL and render a view template.
-  # (HINT: this code is identical to the first route, you just
-  # render a different view template at the end.)
-end
+#   # Some more code to parse the URL and render a view template.
+#   # (HINT: this code is identical to the first route, you just
+#   # render a different view template at the end.)
+# end
 
-get("/:from_currency/:to_currency") do
-  @original_currency = params.fetch("from_currency")
-  @destination_currency = params.fetch("to_currency")
+# get("/:from_currency/:to_currency") do
+#   @original_currency = params.fetch("from_currency")
+#   @destination_currency = params.fetch("to_currency")
 
-  api_url = "https://api.exchangerate.host/convert?access_key=#{ENV.fetch("EXCHANGE_RATE_KEY")}&from=#{@original_currency}&to=#{@destination_currency}&amount=1"
+#   api_url = "https://api.exchangerate.host/convert?access_key=#{ENV.fetch("EXCHANGE_RATE_KEY")}&from=#{@original_currency}&to=#{@destination_currency}&amount=1"
   
-  # Some more code to parse the URL and render a view template.
-end
+#   # Some more code to parse the URL and render a view template.
+# end
