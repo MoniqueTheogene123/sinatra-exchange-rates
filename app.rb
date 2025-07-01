a28dafd9859eadfd3df0d82e0513e035b6af707c
MoniqueTheogene123 <39348407+MoniqueTheogene123@users.noreply.github.com>
2025-07-01 17:17:02 +0000

    no idea

--- app.rb
+++ app.rb
@@ -9,10 +9,8 @@ require "http"
 # define a route for the homepage
 get("/") do
 
-
-
   # Assemble the API url, including the API key in the query string
-  api_url = "https://api.exchangerate.host/list?access_key=#{ENV.fetch(("EXCHANGE_RATE_KEY"))}"
+  api_url = "https://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_RATE_KEY")}"
 
   # Use HTTP.get to retrieve the API data
   @raw_response = HTTP.get(api_url)
@@ -36,4 +34,23 @@ get("/") do
   # Chrome to see what your templates are actually outputting, 
   # and embed as many instance variables as you need to (this 
   # is the new equivalent of pretty-printing everything).
+end
+
+get("/:from_currency") do
+  @original_currency = params.fetch("from_currency")
+
+  api_url = "https://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_RATE_KEY")}"
+  
+  # Some more code to parse the URL and render a view template.
+  # (HINT: this code is identical to the first route, you just
+  # render a different view template at the end.)
+end
+
+get("/:from_currency/:to_currency") do
+  @original_currency = params.fetch("from_currency")
+  @destination_currency = params.fetch("to_currency")
+
+  api_url = "https://api.exchangerate.host/convert?access_key=#{ENV.fetch("EXCHANGE_RATE_KEY")}&from=#{@original_currency}&to=#{@destination_currency}&amount=1"
+  
+  # Some more code to parse the URL and render a view template.
 end
