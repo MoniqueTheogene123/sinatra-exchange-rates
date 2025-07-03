7bec193a6a766698c95863d463c5dc0edc0afb60
MoniqueTheogene123 <39348407+MoniqueTheogene123@users.noreply.github.com>
2025-07-03 14:24:16 +0000

    save please

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ require "http"
 
 # define a route for the homepage
 get("/") do
-  @raw_response = HTTP.get("https://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_RATE_KEY").chomp}")
+  @raw_response = HTTP.get("https://api.exchangerate.host/list?access_key=3f7fcf1d090548ea5f5ab1de8c9c6a4f")
 
   @string_response = @raw_response.to_s
 
@@ -19,8 +19,34 @@ get("/") do
   erb(:homepage)
 end
 
-get("/:first_symbol") do
-  @the_symbol = params.fetch("first_symbol").to_s
+get("/:from_currency") do
+  @original_currency = params.fetch("from_currency").to_s
+
+  api_url = "https://api.exchangerate.host/list?access_key=3f7fcf1d090548ea5f5ab1de8c9c6a4f"
+  
+  @string_response = @raw_response.to_s
+
+  @parsed_response = JSON.parse(@string_response)
+  
+  @currencies = @parsed_response.fetch("currencies")
 
   erb(:c_one)
+end
+
+
+get("/:from_currency/:to_currency") do
+  @original_currency = params.fetch("from_currency")
+  @destination_currency = params.fetch("to_currency")
+
+  @api_url = "https://api.exchangerate.host/convert?from=#{from_currency}&to=#{to_currency}&amount=1&access_key=3f7fcf1d090548ea5f5ab1de8c9c6a4f"
+
+  @raw_response = HTTP.get(@api_url)
+  
+  @string_response = @raw_response.to_s
+
+  @parsed_response = JSON.parse(@string_response)
+  
+  @amount = @parsed_response.fetch("result")
+  
+  erb(:c_two)
 end
